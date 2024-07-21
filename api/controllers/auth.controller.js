7f6abb0fb7945abbd16c740df678febd0f615929
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-07-21 15:53:55 +0530

    create error handling middleware and function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,14 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const singup = async (req, res) => {
+import { errorHandle } from "../utils/error.js";
+
+export const singup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+      return next(errorHandle(400, "All fileds are required"));
     }
 
     const salt = bcrypt.genSaltSync(10);
@@ -19,8 +21,8 @@ export const singup = async (req, res) => {
     });
 
     await newUser.save();
-    return res.status(200).json({ message: "Singup successful" });
+    res.status(200).json("Singup successful");
   } catch (err) {
-    return res.status(500).json({ message: err.message });
+    next(err);
   }
 };
--- api/index.js
+++ api/index.js
@@ -26,6 +26,17 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
+// error-handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is running at post 3000");
 });
