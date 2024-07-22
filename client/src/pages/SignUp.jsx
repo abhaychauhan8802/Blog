c227410580d11a8d4d0cd7d9f53210b57be0f32b
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-07-22 09:39:55 +0530

    fix signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -56,7 +56,7 @@ const SignUp = () => {
           </form>
           <div className="flex gap-2 text-sm mt-4">
             <span>Have an account?</span>
-            <Link to="/sing-in" className="text-blue-500">
+            <Link to="/sign-in" className="text-blue-500">
               Sing In
             </Link>
           </div>
