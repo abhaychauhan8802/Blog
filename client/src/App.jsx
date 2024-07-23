c665c287552fdb403d85ba1a9dc07541e9ba6b12
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-07-23 19:54:49 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home, About, Dashboard, Projects, SignUp, SignIn } from "./pages";
-import { Header, FooterCom } from "./components";
+import { Header, FooterCom, PrivateRoute } from "./components";
 
 const App = () => {
   return (
@@ -11,7 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import FooterCom from "./FooterCom";
 import OAuth from "./OAuth";
+import PrivateRoute from "./PrivateRoute";
 
-export { Header, FooterCom, OAuth };
+export { Header, FooterCom, OAuth, PrivateRoute };
