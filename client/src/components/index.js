708533f6d55ee83667bdba70395435c24081bcdb
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-07-24 09:47:04 +0530

    complete sidebar of dashboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,7 @@ import Header from "./Header";
 import FooterCom from "./FooterCom";
 import OAuth from "./OAuth";
 import PrivateRoute from "./PrivateRoute";
+import SideBar from "./Dashboard/SideBar";
+import Profile from "./Dashboard/Profile";
 
-export { Header, FooterCom, OAuth, PrivateRoute };
+export { Header, FooterCom, OAuth, PrivateRoute, SideBar, Profile };
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import { SideBar, Profile } from "../components";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <SideBar />
+      </div>
+      {tab === "profile" && <Profile />}
+    </div>
+  );
 };
 
 export default Dashboard;
